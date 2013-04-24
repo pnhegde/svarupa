4112a5c17968f359e458c6026081a60861090669
PNH <phaneendra.hegde@gmail.com>
2013-04-24 23:35:38 +0530

    create event page added

--- app.py
+++ app.py
@@ -2,16 +2,16 @@
 
 import json
 import datetime
-import bson.json_util
 import urllib2
 import os
 
 from flask import Flask
 from flask import request
 from flask import render_template
-
+import MySQLdb
 
 app = Flask(__name__)  # Creates a Flask application object
+db = MySQLdb.connect("localhost","root","kde","EventMDB" )
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -25,6 +25,15 @@ def about():
 @app.route("/login/", methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
+
+@app.route("/authentication/", methods=['GET', 'POST'])
+def authentication():
+    return ('<h2>Hello</h2>')
+
+@app.route("/createNewEvent/", methods=['GET', 'POST'])
+def createNewEvent():
+    return render_template('create_event.html')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
