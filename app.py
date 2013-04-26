0bc0870967090a7ebeb4be345c36678aa0a2d886
PNH <phaneendra.hegde@gmail.com>
2013-04-26 19:28:07 +0530

    fb oauth added

--- app.py
+++ app.py
@@ -10,7 +10,32 @@ from flask import request
 from flask import render_template
 import MySQLdb
 
+from flask import Flask, redirect, url_for, session, request
+from flask_oauth import OAuth
+
+
+SECRET_KEY = 'a3eb0e9b167d5524d1c84fe68bdc2271'
+DEBUG = True
+FACEBOOK_APP_ID = '292895004174602'
+FACEBOOK_APP_SECRET = 'a3eb0e9b167d5524d1c84fe68bdc2271'
+
+
 app = Flask(__name__)  # Creates a Flask application object
+app.debug = DEBUG
+app.secret_key = SECRET_KEY
+oauth = OAuth()
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': 'email'}
+)
+
+
 db = MySQLdb.connect("localhost","root","kde","EventMDB" )
 cursor = db.cursor()
 authenticated = False
@@ -26,7 +51,34 @@ def about():
 
 @app.route("/login/", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
+
+
+@app.route('/login/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['oauth_token'] = (resp['access_token'], '')
+    me = facebook.get('/me')
+    return 'Logged in as id=%s name=%s redirect=%s' % \
+        (me.data['id'], me.data['name'], request.args.get('next'))
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('oauth_token')
+
+
+@app.route("/logout/", methods=['GET', 'POST'])
+def logout():
+    global authenticated
+    authenticated = False
+    return index()
 
 @app.route("/authentication/", methods=['GET', 'POST'])
 def authentication():
